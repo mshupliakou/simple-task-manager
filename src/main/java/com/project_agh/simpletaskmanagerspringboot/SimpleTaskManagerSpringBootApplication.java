ba8768d6e1a940e3407b6f168675653023491c0a
mshupliakou <mikhail.shupliakou@gmail.com>
2026-05-16 12:27:59 +0200

    initial commit

---
+++ src/main/java/com/project_agh/simpletaskmanagerspringboot/SimpleTaskManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.project_agh.simpletaskmanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTaskManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTaskManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project_agh/simpletaskmanagerspringboot/SimpleTaskManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project_agh.simpletaskmanagerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTaskManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
